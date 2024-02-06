1f20efa5bb098dc14d450364346cc00545e6e1b9
UserKorns1202 <kornskids@gmail.com>
2024-02-06 09:39:03 -0500

    Platform Work

---
+++ Assets/CameraController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraController : MonoBehaviour
+{
+    GameObject _player; //player reference
+    Vector3 _offset; //distance between player and camera
+
+    // Use this for initialization
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+
+        //offset = the difference in player position and camera position
+        _offset = _player.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //camera position = player position - offset
+        transform.position = _player.transform.position - _offset;
+    }
+}
