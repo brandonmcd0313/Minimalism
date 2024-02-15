1304cf4470487cde5f5bce5a46f9f8fb594cd4f9
UserKorns1202 <kornskids@gmail.com>
2024-02-14 23:51:36 -0500

    Blessings of Pollito
    Amen

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -16,8 +16,12 @@ public class PlayerController : MonoBehaviour
     private IInteractable interactableInRange;
     public bool canDoubleJump = true;
     int doubleJump = 0;
-    public GameObject background;
-    public bool canMove = true; 
+    public GameObject background, textBoxBack;
+    public TextMeshProUGUI textBox;
+    public bool canMove = true;
+    public Sprite playerBox, pollitoBox;
+    //fonts for tmpro
+    public TMP_FontAsset regFont, pollitoFont;
 
     void Start()
     {
@@ -57,6 +61,50 @@ public class PlayerController : MonoBehaviour
         rb2d.velocity = new Vector2(Mathf.Clamp(rb2d.velocity.x, -10f, 10f), rb2d.velocity.y);
 
        
+    }
+
+    public void HideText()
+    {
+        textBoxBack.SetActive(false);
+        canMove = true;
+    }
+
+    public void Speak(string words, Sprite textBack)
+    {
+        textBoxBack.gameObject.SetActive(true);
+        textBox.text = "";
+        if (textBack == pollitoBox)
+        {
+            textBox.color = Color.black;
+            textBox.font = pollitoFont;
+            textBoxBack.transform.localScale = new Vector3(40f, 40f, 40f);
+        }
+        else
+        {
+            textBox.color = Color.white;
+            textBox.font = regFont;
+            textBoxBack.transform.localScale = new Vector3(60f, 60f, 60f);
+        }
+
+        textBoxBack.GetComponent<SpriteRenderer>().sprite = textBack;
+
+        //turn off player movement and kill all player movement
+        canMove = false;
+        rb2d.velocity = Vector2.zero;
+
+
+        //text scrolling effect
+        StartCoroutine(ScrollText(words));
+    }
+
+    IEnumerator ScrollText(string words)
+    {
+        textBox.text = "";
+        foreach (char letter in words.ToCharArray())
+        {
+            textBox.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 
     private void Flip()
@@ -135,25 +183,26 @@ public class PlayerController : MonoBehaviour
             //Kill all player movement
             rb2d.velocity = Vector2.zero;
 
-            StartCoroutine(Pollito());
-            //Hex Code for new Camera Background color: 73706D
+            
 
 
             //Change the background to pollitoBackground
             background.gameObject.SetActive(false);
+            StartCoroutine(Pollito());        
         }
     }
 
     IEnumerator Pollito()
     {
-        yield return null;
         while (transform.position != new Vector3(485, 189.8448f, 0) || Camera.main.orthographicSize != 15) {
             //Gradually move player to 480 189.8448 using moveTowards
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(485, 189.8448f, 0), 0.1f);
 
             //gradually zoom camera to orthographic size 10
             Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, 15, 0.1f);
+            yield return new WaitForSeconds(0.05f);
         }
+        Speak("MY CHILD, IT IS I... POLLITO...", pollitoBox);
     }
 
 
